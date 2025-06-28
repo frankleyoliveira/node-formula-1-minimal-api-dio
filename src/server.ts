b58f7d0aa936b3765870477351b6cf4060e155cc
Frankley Oliveira <frankleyj.o@gmail.com>
2025-06-28 17:43:35 -0300

    refactor code

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import { drivers, teams } from './data'
 
 const server = fastify({ logger: true })
 
@@ -7,33 +8,12 @@ server.register(cors, {
   origin: '*',
 })
 
-const teams = [
-  { id: 1, name: 'McLaren', base: 'Woking, United Kingdom' },
-  { id: 2, name: 'Mercedes', base: 'Brackley, United Kingdom' },
-  { id: 3, name: 'Red Bull Racing', base: 'Milton Keynes, United Kingdom' },
-  { id: 4, name: 'Ferrari', base: 'Maranello, Italy' },
-  { id: 5, name: 'Alpine', base: 'Enstone, United Kingdom' },
-  { id: 6, name: 'Aston Martin', base: 'Silverstone, United Kingdom' },
-  { id: 7, name: 'Alfa Romeo Racing', base: 'Hinwil, Switzerland' },
-  { id: 8, name: 'AlphaTauri', base: 'Faenza, Italy' },
-  { id: 9, name: 'Williams', base: 'Grove, United Kingdom' },
-  { id: 10, name: 'Haas', base: 'Kannapolis, United States' },
-  { id: 11, name: 'Uralkali Haas F1 Team', base: 'Banbury, United Kingdom' },
-  { id: 12, name: 'Scuderia Toro Rosso', base: 'Faenza, Italy' },
-]
-
-const drivers = [
-  { id: 1, name: 'Max Verstappen', team: 'Red Bull Racing' },
-  { id: 2, name: 'Lewis Hamilton', team: 'Ferrari' },
-  { id: 2, name: 'Lando Norris', team: 'McLaren' },
-]
-
-server.get('/teams', async (request, response) => {
+server.get('/api/teams', async (_, response) => {
   response.type('application/json').code(200)
   return { teams }
 })
 
-server.get('/drivers', async (request, response) => {
+server.get('/api/drivers', async (_, response) => {
   response.type('application/json').code(200)
   return { drivers }
 })
@@ -43,7 +23,7 @@ interface DriverParams {
 }
 
 server.get<{ Params: DriverParams }>(
-  '/drivers/:id',
+  '/api/drivers/:id',
   async (request, response) => {
     const id = parseInt(request.params.id)
     const driver = drivers.find((d) => d.id === id)
@@ -58,6 +38,8 @@ server.get<{ Params: DriverParams }>(
   }
 )
 
-server.listen({ port: 3333 }, () => {
-  console.log('Server init')
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3333
+
+server.listen({ port }, () => {
+  console.log(`Server started on port ${port}`)
 })
